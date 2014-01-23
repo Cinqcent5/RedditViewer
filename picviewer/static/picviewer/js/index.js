e53ad7dd98beece59a0b7b476cda5fd2dd01275b
David Zou <davidczou@gmail.com>
2014-01-23 00:09:26 -0500

    Added function to retrieve and display images

--- picviewer/static/picviewer/js/index.js
+++ picviewer/static/picviewer/js/index.js
@@ -9,9 +9,31 @@ function sendRequest() {
 function stateChangeHandlers() {
     if (this.readyState == 4) {
         if (this.status == 200) {
-            document.write(this.responseText);
+            parseJSON(this.responseText);
         } else {
             document.write("Connection Error");
+        }
+    }
+}
+
+function parseJSON(responseText) {
+    var width = 350;
+    var maxWidth = window.screen.width - 100;
+    var maxColumns = Math.floor(maxWidth / (width + 20));
+    var currentColumn = 0;
+    var responseJSON = JSON.parse(responseText);
+    var submissions = responseJSON.data.children;
+    for (var i = 0; i < submissions.length; i++) {
+        var url = submissions[i].data.url;
+        // Only display the link if it's a image
+        if (url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+            var imageHTML = "<a class='imageLink' href=" + url + " target=_blank><img src=" + url + " width=" + width + "></a>";
+            if (currentColumn > maxColumns) {
+                imageHTML += "<br/>";
+                currentColumn=0;
+            }
+            document.body.innerHTML += imageHTML;
+            currentColumn = currentColumn + 1;
         }
     }
 }
