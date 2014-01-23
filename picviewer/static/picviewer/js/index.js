952c1ceff56e67177f9eaee24044b6e9346408cd
David Zou <davidczou@gmail.com>
2014-01-23 01:17:43 -0500

    Add ability to load more images

--- picviewer/static/picviewer/js/index.js
+++ picviewer/static/picviewer/js/index.js
@@ -1,7 +1,13 @@
 var base_link = "http://www.reddit.com/";
+var lastFullname = "";
 function sendRequest() {
+
+    $("#loadingGif").show();
+    $("#loadMoreButton").hide();
+
     var httprequest = new XMLHttpRequest();
-    httprequest.open("GET", base_link + ".json", true);
+
+    httprequest.open("GET", base_link + ".json?limit=30&after=" + lastFullname, true);
     httprequest.send();
     httprequest.onreadystatechange = stateChangeHandlers;
 }
@@ -18,9 +24,6 @@ function stateChangeHandlers() {
 
 function parseJSON(responseText) {
     var width = 350;
-    var maxWidth = window.screen.width - 100;
-    var maxColumns = Math.floor(maxWidth / (width + 20));
-    var currentColumn = 0;
     var responseJSON = JSON.parse(responseText);
     var submissions = responseJSON.data.children;
     for (var i = 0; i < submissions.length; i++) {
@@ -28,12 +31,14 @@ function parseJSON(responseText) {
         // Only display the link if it's a image
         if (url.match(/\.(jpeg|jpg|gif|png)$/) != null) {
             var imageHTML = "<a class='imageLink' href=" + url + " target=_blank><img src=" + url + " width=" + width + "></a>";
-            if (currentColumn > maxColumns) {
-                imageHTML += "<br/>";
-                currentColumn=0;
-            }
-            document.body.innerHTML += imageHTML;
-            currentColumn = currentColumn + 1;
+            document.getElementById("gallery").innerHTML += imageHTML;
         }
     }
+    $("#loadingGif").hide();
+    $("#loadMoreButton").show();
+
+    //return the fullname of the last submission fetched
+    var lastSubmission = submissions[submissions.length - 1];
+    lastFullname = lastSubmission.kind + "_" + lastSubmission.data.id;
+
 }
